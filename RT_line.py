6f0ad6037e038ffa3271d98fcf914d519bd1b5bc
Boris Minasenko <bbminasenko@edu.hse.ru>
2022-06-18 13:21:48 -0400

    delete metadata

--- RT_line.py
+++ RT_line.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
 import numpy as np
 import matplotlib.pyplot as plt
 import math
